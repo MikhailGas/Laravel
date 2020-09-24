d2c8b85a4eac63e12469d54b3f1b013bf841c2d8
Mikhail <m-gasanov@ya.ru>
2020-09-24 23:32:18 +0500

    Lesson1

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//Домашняя работа к уроку №1
 
 Route::view('/', 'index');
 Route::view('/about', 'about');
