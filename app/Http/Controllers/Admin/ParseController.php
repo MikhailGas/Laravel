abc2a23d7b2ec05fcb4e7a62e4088bbca9c9d017
Mikhail <m-gasanov@ya.ru>
2020-11-11 07:10:49 +0500

    prod

---
+++ app/Http/Controllers/Admin/ParseController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Http\Controllers\Controller;
+use App\Jobs\NewsParsing;
+
+use App\Models\Source;
+use App\Services\XMLParserService;
+
+use Illuminate\Http\Request;
+
+class ParseController extends Controller
+{
+    public function index(XMLParserService $parse){
+        
+        $resources = Source::all();
+        
+       
+        foreach($resources as $source){
+            $parse->saveNews($source->link);
+        }
+    
+        return redirect()->route('news.categories');
+        
+    }
+}
